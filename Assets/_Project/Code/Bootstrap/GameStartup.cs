fe5f0bf995151a15af0a5a229349b5ad64030370
alex_r <rio99rom@gmail.com>
2026-05-25 11:16:28 +0300

    feat(bootstrap): set up minimal VContainer lifetime scope with logging startup

---
+++ Assets/_Project/Code/Bootstrap/GameStartup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using VContainer.Unity;
+
+namespace Minesweeper.Bootstrap
+{
+    public class GameStartup : IStartable
+    {
+        public void Start()
+        {
+            Debug.Log("Game started");
+        }
+    }
+}
